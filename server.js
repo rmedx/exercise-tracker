49375ae08fe0789243681b04d238bf65910161b9
rmedx <65210483+rmedx@users.noreply.github.com>
2021-08-26 22:10:34 -0400

    tentative finish

--- server.js
+++ server.js
@@ -21,11 +21,12 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // schema for user
 const userSchema = new Schema({
   username: {type: String, unique: true, required: true},
-  exercises: [{
-    description: {type: String, required: true},
-    duration: { type: Number, required: true },
-    date: {type: String, required: true},
-  }]
+  // exercises: [{
+  //   date: {type: String, required: true},
+  //   duration: { type: Number, required: true },
+  //   description: {type: String, required: true}
+  // }]
+  exercises: Array
 });
 // model for user
 const User = mongoose.model('User', userSchema);
@@ -65,7 +66,7 @@ app.post('/api/users/:_id/exercises', (req, res) => {
   // console.log(typeof(req.body[':_id']));
   let outUserId = req.body[':_id'];
   let outDescription = req.body.description;
-  let outDuration = req.body.duration;
+  let outDuration = Number.parseInt(req.body.duration);
   let outDate = new Date (req.body.date);
   if (!outDate) {
     outDate = new Date().toString().substring(0, 15);
@@ -73,42 +74,71 @@ app.post('/api/users/:_id/exercises', (req, res) => {
     outDate = new Date(outDate).toString().substring(0, 15);
   }
   let newExercise = {
-    description: outDescription,
+    date: outDate,
     duration: outDuration,
-    date: outDate
+    description: outDescription
   };
   // console.log(newExercise);
   User.findById(outUserId, (err, individual) => {
     if (err) {
       return console.log("error finding user by id");
-    } else {
-      // console.log(individual);
-      // console.log(individual.username);
-      // console.log(individual._id);
-      console.log(individual.exercises);
-      individual.exercises.push(newExercise);
-      individual.save((err, savedIndividual) => {
-        if (err) {
-          return console.log("error saving user after exercises update");
-        } else {
-          res.json({
-            _id: outUserId,
-            username: individual.username,
-            description: outDescription,
-            duration: outDuration,
-            date: outDate
-          });
-        }
-      })
     }
+    individual.exercises.push(newExercise);
+    individual.save((err, savedIndividual) => {
+      if (err) {
+        return console.log("error saving user after exercises update");
+      }
+      res.json({
+        _id: outUserId,
+        username: individual.username,
+        date: outDate,
+        duration: outDuration,
+        description: outDescription
+      });
+    })
   });
 });
 
 // get log of exercises
-// app.get('/api/users/:_id/logs', (req, res) => {
-//   let inputId = req.params._id;
-//   console.log(inputId);
-// })
+app.get('/api/users/:_id/logs', (req, res) => {
+  let inputId = req.params._id;
+  let from;
+  let to;
+  if (req.query.from && req.query.to) {
+    from = new Date(req.query.from);
+    to = new Date(req.query.to);
+  }
+  let limit = req.query.limit;
+  User.findById(inputId, (err, individual) => {
+    if (err) {
+      return console.log("error finding user for logs");
+    }
+    console.log(individual.exercises);
+    let exercisesCopy = JSON.parse(JSON.stringify(individual.exercises));
+    console.log(exercisesCopy);
+    if (from && to) {
+      exercisesCopy = exercisesCopy.filter(d => {
+        let temp = new Date(d.date);
+        return (from <= temp && temp <= to);
+      });
+    }
+    let length = exercisesCopy.length;
+    if (limit && limit < length) {
+      exercisesCopy = exercisesCopy.slice(0, limit);
+    }
+    console.log(exercisesCopy);
+    let countOutput = exercisesCopy.length;
+    res.json({
+      _id: individual._id,
+      username: individual.username,
+      count: countOutput,
+      log: exercisesCopy
+    });
+  });
+});
+
+// get log of exercises with limit or date range
+app.get('/api/users/:_id/logs?')
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
